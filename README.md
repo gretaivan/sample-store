05007f21e15248322e6aebd2f1063e1daefc69ca
gretaivan <greta.ivan91@gmail.com>
2022-11-15 19:43:48 +0000

    adds ejs, and implements signup page with partial head, header and footer templates

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 ## Technology
 
 - Express
+- EJS
--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const authRoutes = require('./routes/auth.routes');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
 // keep commented for now
 // app.use(express.json());
