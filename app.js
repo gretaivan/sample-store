ff048986d6cdd37f542ccfa0c631bc6a4bcda02a
gretaivan <greta.ivan91@gmail.com>
2022-11-18 13:16:31 +0000

    adds base of styling

--- app.js
+++ app.js
@@ -8,11 +8,12 @@ const PORT = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static('public'));
 // keep commented for now
 // app.use(express.json());
 // app.use(cors());
 app.use(authRoutes);
 
 
-app.listen(PORT, () => console.log(`[SERVER]: Server has started up successfuly...`));
+app.listen(PORT, () => console.log(`[SERVER]: Server has started up successfuly on PORT:${PORT}`));
 
